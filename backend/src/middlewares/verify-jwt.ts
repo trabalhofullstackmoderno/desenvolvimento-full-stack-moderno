ebd8830e865f2f87b14202256496c62311e51fc3
Pedro Dalalio <pedrodalalio.dev@getzip.com.br>
2025-10-29 10:09:29 -0300

    console error

--- backend/src/middlewares/verify-jwt.ts
+++ backend/src/middlewares/verify-jwt.ts
@@ -1,9 +1,10 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
-export async function verifyJWT(request: FastifyRequest, reply: FastifyReply){
+export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
   try {
-    await request.jwtVerify()
+    await request.jwtVerify();
   } catch (error) {
-    return reply.status(401).send({ message: "Unauthorized." })
+    console.error(error);
+    return reply.status(401).send({ message: "Unauthorized." });
   }
 }
